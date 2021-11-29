408fbb2cf45e5474d80a0947187fc8692a170a74
abrahamkivosh <abrahamkivosh@gmail.com>
2021-11-29 12:16:00 +0300

    modified sidebar

--- resources/views/recommend/includes/sidebar.blade.php
+++ resources/views/recommend/includes/sidebar.blade.php
@@ -9,13 +9,13 @@
                     <a href="javascript:void(0)" class="dropdown-toggle u-dropdown link hide-menu" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ auth()->user()->name }} <span class="caret"></span></a>
                     <div class="dropdown-menu animated flipInY">
                         <!-- text-->
-                        <a href="javascript:void(0)" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
+                        <a href="{{ route('show.user',auth()->user()) }}" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
                         <!-- text-->
                         <!-- text-->
                         <!-- text-->
                         <div class="dropdown-divider"></div>
                         <!-- text-->
-                        <a href="javascript:void(0)" class="dropdown-item"><i class="ti-settings"></i> Account Setting</a>
+                        <a href="{{ route('show.user',auth()->user()) }}" class="dropdown-item"><i class="ti-settings"></i> Account Setting</a>
                         <!-- text-->
                         <div class="dropdown-divider"></div>
                         <!-- text-->
